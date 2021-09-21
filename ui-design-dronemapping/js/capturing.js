6fc1772a68207db2c476c4ad77bbc5b2ecbee37f
Xytrical <ixandermc@gmail.com>
2021-09-21 15:53:13 +0200

    added all charts

--- ui-design-dronemapping/js/capturing.js
+++ ui-design-dronemapping/js/capturing.js
@@ -1,41 +1,53 @@
-const batteryPercentage = 100;
-const storage = 10;
-var batteryChart, storageChart;
+const batteryPercentage = {
+    value: 30,
+    total: 100
+};
+const storage = {
+    value: 10,
+    total: 100
+};
+const imagesTaken = {
+    value: 50,
+    total: 150
+};
+var batteryChart, storageChart, imagesChart;
 
 $(document).ready(function() {
-    renderCharts(0, 0);
+    renderCharts(0, 0, 0);
     setTimeout(function() { 
-        batteryChart.set(batteryPercentage);
-        storageChart.set(storage);
-        $(':root').css('--fill-battery', correctChart(batteryPercentage, 'battery'));
-        $(':root').css('--fill-storage', correctChart(storage, 'storage'));
+        batteryChart.set(batteryPercentage.value);
+        storageChart.set(storage.value);
+        imagesChart.set(imagesTaken.value);
+        $(':root').css('--fill-battery', correctChart(batteryPercentage.value, 'battery', batteryPercentage.total));
+        $(':root').css('--fill-storage', correctChart(storage.value, 'storage', storage.total));
+        $(':root').css('--fill-images', correctChart(imagesTaken.value, 'images', imagesTaken.total));
     }, 200)
 });
 
-const renderCharts = function(batteryValue, storageValue) {
-    batteryChart = drawChart(batteryValue, 'battery');
-    storageChart = drawChart(storageValue, 'storage');
+const renderCharts = function(batteryValue, storageValue, imagesValue) {
+    batteryChart = drawChart(batteryValue, 'battery', batteryPercentage.total);
+    storageChart = drawChart(storageValue, 'storage', storage.total);
+    imagesChart = drawChart(imagesValue, 'images', imagesTaken.total)
 }
 
-const correctChart = function(value, type) {
-    if ((type == 'battery' && value < 33) || (type == 'storage' && value > 66)) return '#F15454'; // 🔴
-    else if ((type == 'battery' || 'storage') && (value >= 33 && value <= 66)) return '#FFF172'; // 🟡
-    else return '#05FF00'; // 🟢
+const correctChart = function(value, type, totalValue) {
+    let dividedValue = (totalValue / 3);
+
+    if (type == 'battery' ) {
+        if (value < dividedValue) return '#F15454'; // 🔴
+        else if (value >= dividedValue && value <= dividedValue * 2) return '#FFF172'; // 🟡)
+        else return '#05FF00'; // 🟢
+    } else {
+        if (value > dividedValue * 2) return '#F15454'; // 🔴
+        else if (value >= dividedValue && value <= dividedValue * 2) return '#FFF172'; // 🟡)
+        else return '#05FF00'; // 🟢
+    }
 }
 
-const drawChart = function(value, type) {
+const drawChart = function(value, type, total) {
     return new ldBar(`.${type}-chart`, {
-        "stroke": correctChart(value, type),
+        "stroke": correctChart(value, type, total),
         "stroke-width": 6,
         "value": value
     });
 }
-
-
-var progressbar = new ldBar('progressbar', {
-    "stroke": correctChart(value, type),
-    "stroke-width": 6,
-    "value": value
-});
-
-
